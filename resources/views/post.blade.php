c9e54dd03867a623829c2faf8b5320ea9680da4b
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-04-29 00:39:14 +0700

    :sparkle: Requiring all fields

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,12 +9,12 @@
             @csrf
             <div class="form-group">
                 <label for="name">ชื่อที่จะปรากฏ</label>
-                <input type="text" class="form-control" id="name" name="name" maxlength="50">
+                <input type="text" class="form-control" id="name" name="name" maxlength="50" required>
                 <small>จำกัด 50 ตัวอักษร</small>
             </div>
             <div class="form-group">
                 <label for="wish">ข้อความอวยพร</label>
-                <textarea class="form-control" id="wish" name="wish" rows="3" maxlength="240"></textarea>
+                <textarea class="form-control" id="wish" name="wish" rows="3" maxlength="240" required></textarea>
                 <small>จำกัด 280 ตัวอักษร</small>
             </div>
             <button type="submit" class="btn btn-primary">อวยพรวันเกิด!</button>
