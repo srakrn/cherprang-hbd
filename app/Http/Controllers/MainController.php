9133fe979b1801d87b4f2aa2fffd9c7cacece63b
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-04-29 00:35:36 +0700

    :sparkle: Better routing

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -20,6 +20,6 @@ class MainController extends Controller
     public function savePost(Request $request){
         $wish = \App\Wish::create($request->all());
         $wishes = \App\Wish::where('hidden', 0)->orderBy('created_at', 'desc')->get();
-        return view('wishes', ['wishes' => $wishes]);
+        return redirect('/');
     }
 }
