26f97adc0a4f1fe01143558f6e1d58e92bc10b3b
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-04-29 17:22:38 +0700

    :pencil: Google Meta tag

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,6 +7,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="ร่วมเขียนข้อความอวยพรวันเกิดให้กับเฌอปราง BNK48 ได้ที่เว็บไซต์นี้" />
     <link rel="stylesheet" href="{{ asset('css/main.css') }}"></link>
 </head>
 <body>
