28a2e0eaf0df05f16cabcd620e8070a33d899efb
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-04-29 17:15:06 +0700

    :bento: Website header and mobile viewport

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,6 +6,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5mdXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}"></link>
 </head>
 <body>
@@ -13,7 +14,7 @@
     <div class="container">
         <div class="row">
             <div class="col col-lg-3 sidebar text-white">
-                <h1>HAPPY BIRTHDAY CHERPRANG</h1>
+                <h1>#HBD<br/><b>CHERPRANG</b><br/>BNK48</h1>
                 <p>ร่วมส่งข้อความอวยพรวันเกิดให้กับเฌอปราง BNK48 ได้ที่เว็บไซต์นี้ หรือร่วมทวิตติดแท็ก #HBDCherprangBNK48</p>
                 @yield('sidebar')
                 <hr/>
