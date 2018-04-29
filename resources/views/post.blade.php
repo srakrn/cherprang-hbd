e8ea1b72577c5b7e3cdeee4a25b962071d0f5538
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-04-29 22:02:56 +0700

    :sparkle: Character count

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -20,12 +20,12 @@
             <div class="form-group">
                 <label for="name">ชื่อที่จะปรากฏ</label>
                 <input type="text" class="form-control" id="name" name="name" maxlength="50" required>
-                <small>จำกัด 50 ตัวอักษร</small>
+                <small id="name-count">เหลือ 50 ตัวอักษร</small>
             </div>
             <div class="form-group">
                 <label for="wish">ข้อความอวยพร</label>
-                <textarea class="form-control" id="wish" name="wish" rows="3" maxlength="240" required></textarea>
-                <small>จำกัด 280 ตัวอักษร</small>
+                <textarea class="form-control" id="wish" name="wish" rows="3" maxlength="280" required></textarea>
+                <small id="wish-count">เหลือ 280 ตัวอักษร</small>
             </div>
             <button type="submit" class="btn btn-primary">อวยพรวันเกิด!</button>
         </form>
@@ -35,4 +35,30 @@
 
 @section('sidebar')
 <a class="btn btn-light" style="width: 100%" href="..">ดูคำอวยพร</a>
+@endsection
+
+@section('footer')
+<script>
+$('#wish').keyup(function () {
+  var max = 280;
+  var len = $(this).val().length;
+  if (len > max) {
+    $('#wish-count').text('เกินความยาวแล้วจ้า~ จะกดส่งไม่ไปนะ');
+  } else {
+    var char = max - len;
+    $('#wish-count').text('เหลือ ' + char + ' ตัวอักษร');
+  }
+});
+$('#name').keyup(function () {
+  var max = 50;
+  var len = $(this).val().length;
+  if (len > max) {
+    $('#name-count').text('เกินความยาวแล้วจ้า~ จะกดส่งไม่ไปนะ');
+  } else {
+    var char = max - len;
+    $('#name-count').text('เหลือ ' + char + ' ตัวอักษร');
+  }
+});
+
+</script>
 @endsection
