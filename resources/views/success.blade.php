0d3b3593d2f2d083db0f367430625ad99cfc3c65
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-04-29 18:28:48 +0700

    :pencil: #happyCHERday as main hashtag

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -6,7 +6,7 @@
             <h1>ร่วมอวยพรให้เฌอปรางสำเร็จ!</h1>
             <p>ขอบคุณที่ร่วมอวยพรวันเกิดให้เฌอปราง สามารถคลิกที่ปุ่มทวิต เพื่อทวิตคำอวยพรให้เฌอปรางได้ด้วยนะ
             <a class="twitter-share-button"
-                href="https://twitter.com/intent/tweet?url=hbdcherprang.herokupp.com&text={{ session('wish')->wish}}%20#HBDCherprangBNK48"
+                href="https://twitter.com/intent/tweet?url=hbdcherprang.herokupp.com&text={{ session('wish')->wish}}%20#happyCHERday%20#HBDCherprangBNK48"
             >
             Tweet</a></p>
             <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
