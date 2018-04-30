6a5ecd94930db05fd7af77137c23219b6129ec16
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-04-30 08:21:59 +0700

    :sparkle: Secret view API (for both shown and hidden message)

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,12 +6,15 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public static function list(){
-        return \App\Wish::where('hidden', 0)->orderBy('created_at', 'desc')->get();
-    }
-    
-    public function save(Request $request){
-        $wish = \App\Wish::create($request->all());
-        return $wish;
+    public static function list(Request $request){
+        if($request->all==1){
+            return \App\Wish::orderBy('created_at', 'desc')->get();
+        }
+        else if($request->hidden==1){
+            return \App\Wish::where('hidden', 1)->orderBy('created_at', 'desc')->get();
+        }
+        else{
+            return \App\Wish::where('hidden', 0)->orderBy('created_at', 'desc')->get();
+        }
     }
 }
